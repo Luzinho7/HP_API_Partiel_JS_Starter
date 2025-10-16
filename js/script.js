5b32835d42ae0ea78c8e05aaad10828ddf230237
Lucas <lucas.fernandes3319@gmail.com>
2025-10-16 11:41:01 +0200

    Commit pour partie 2 étape 2. sont trier par maison

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const API_URL = 'https://hp-api.onrender.com/api/characters';
 const container = document.querySelector('.characters');
 
+let charac = [];
+let currentFilter = null;
+
 function maison(house){
     switch(house){
         case 'Gryffindor': return 'gryffondor';
@@ -22,14 +25,46 @@ function personnage(perso){
     </div>
   `;
 }
+function renderList(list){
+  container.innerHTML = list.map(personnage).join('');
+}
 
+function filtreMaison(){
+    const maisonElements = document.querySelectorAll('.houses .house');
+    if(!maisonElements.length) return;
+
+    maisonElements.forEach(ma => {
+        ma.addEventListener('click', () => {
+            const img = ma.querySelector('img');
+            const nomMaison = img ? img.alt : ma.dataset.house;
+            const classe = maison(nomMaison);
+
+            if(currentFilter === classe){
+                currentFilter = null;
+                maisonElements.forEach(m=> m.classList.remove('active'));
+                renderList(charac);
+                return;
+            }
+            currentFilter = classe;
+            maisonElements.forEach(m=> m.classList.remove('active', m === ma));
+            const listeFiltree = charac
+            .filter(f => maison(f.house) === classe)
+            .sort((a,b) => a.name.localeCompare(b.name));
+
+            renderList(listeFiltree);
+
+        });
+    });
+}
 async function loadCharacters() {
 try{
     const res = await fetch(API_URL);
     if(!res.ok) throw new Error('Erreur');
     const data = await res.json();
     const first12 = data.slice(0,12);
-    container.innerHTML = first12.map(personnage).join('');
+    charac = first12;
+    renderList(first12);
+    filtreMaison();
 }catch(e){
     container.innerHTML = '<p>Erreur</p>';
 }
